7f329b6ecf0e99c56ef30f0af37c0ca38d5b2b92
cyranodebergerac <eric.artigala@gmail.com>
2014-05-27 16:40:04 +0200

    test

---
+++ models/0.py
@@ -0,0 +1,19 @@
+from gluon.storage import Storage
+settings = Storage()
+
+settings.migrate = True
+settings.title = 'Mon appli Web2Py'
+settings.subtitle = 'R\xc3\xa9alis\xc3\xa9 par Eric'
+settings.author = 'Eric Artigala'
+settings.author_email = 'you@example.com'
+settings.keywords = ''
+settings.description = 'un d\xc3\xa9but'
+settings.layout_theme = 'Default'
+settings.database_uri = 'sqlite://storage.sqlite'
+settings.security_key = '59159bb4-c7be-4df1-857d-140187644b2b'
+settings.email_server = 'localhost'
+settings.email_sender = 'you@example.com'
+settings.email_login = ''
+settings.login_method = 'local'
+settings.login_config = ''
+settings.plugins = []
